da97c81f195d66f10f5ce71ee1a02ee26e215a69
rwendell <rwendell@iastate.edu>
2018-02-13 19:28:59 -0600

    updated sql buildtable script

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -1,16 +1,19 @@
-create table Users(
-UserID char (15) NOT NULL,
-Username varchar (15) NOT NULL,
-Hash nvarchar(128) NOT NULL,
-Salt nvarchar(10) NOT NULL,
-Admin bool NOT NULL,
-primary key (UserID)
-);
+CREATE TABLE `artboard` (
+  `ArtboardID` int(11) NOT NULL AUTO_INCREMENT,
+  `Artboard_Name` varchar(15) DEFAULT NULL,
+  `UserID` int(11) NOT NULL,
+  PRIMARY KEY (`ArtboardID`),
+  UNIQUE KEY `artboard_ArtboardName_uindex` (`Artboard_Name`),
+  KEY `UserID_idx` (`UserID`),
+  CONSTRAINT `UserID` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf16;
 
-create table ArtBoard(
-UserID char (15) NOT NULL references Users(UserID),
-ArtboardID char (15) NOT NULL,
-ArtboardName varchar (15),
-primary key (ArtboardID)
-);
-
+CREATE TABLE `user` (
+  `UserID` int(11) NOT NULL AUTO_INCREMENT,
+  `Username` varchar(15) NOT NULL,
+  `Hash` varchar(128) NOT NULL,
+  `Salt` varchar(8) NOT NULL,
+  `Admin` tinyint(1) NOT NULL,
+  PRIMARY KEY (`UserID`),
+  UNIQUE KEY `users_Username_uindex` (`Username`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf16;
