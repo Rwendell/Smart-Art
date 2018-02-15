e8b2152d0f3c2084fd70737253967e1dd5d90be8
rwendell <rwendell@iastate.edu>
2018-02-14 19:26:11 -0600

    added user login and password changing functionality

--- server/src/main/java/com/smartart/server/UserController.java
+++ server/src/main/java/com/smartart/server/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @Controller    // This means that this class is a Controller
-@RequestMapping(path="/user") // This means URL's start with /user (after Application path)
+@RequestMapping(path = "/user") // This means URL's start with /user (after Application path)
 
 public class UserController {
 
@@ -17,31 +17,29 @@ public class UserController {
 
     private UserRepository UserRepository;
 
-    @PostMapping(path= "/add") //Map ONLY POST Requests
-    public @ResponseBody String addNewUser (@RequestParam String username
+    @PostMapping(path = "/add") //Map ONLY POST Requests
+    public @ResponseBody
+    String addNewUser(@RequestParam String username
             , @RequestParam String password) {
         // @ResponseBody means the returned String is the response, not a view name
         // @RequestParam means it is a parameter from the GET or POST request
 
 
-
         try {
             //noinspection ResultOfMethodCallIgnored    This makes sure the warning is suppresed
             UserRepository.findByUsername(username).getUsername();
-            }
-            catch (NullPointerException ex)
-            {
-                User n = new User();
-                n.setUsername(username);  //sets Username
-                n.setAdmin(0);      //default to 0 -> false
-                String salt_string = RandomStringUtils.random(8);
-                n.setSalt(salt_string);
-                String passSalt = salt_string + password;
-                String hashed = DigestUtils.sha256Hex(passSalt);
-                n.setHash(hashed);
-                UserRepository.save(n);
-                return "Saved New User";
-            }
+        } catch (NullPointerException ex) {
+            User n = new User();
+            n.setUsername(username);  //sets Username
+            n.setAdmin(0);      //default to 0 -> false
+            String salt_string = "????????";//RandomStringUtils.random(8); for some reason this isn't working --> stores salt as ????????
+            n.setSalt(salt_string);
+            String passSalt = salt_string + password;
+            String hashed = DigestUtils.sha256Hex(passSalt);
+            n.setHash(hashed);
+            UserRepository.save(n);
+            return "Saved New User";
+        }
 
 
         return "Username Taken";
@@ -57,6 +55,57 @@ public class UserController {
             If they match, the password is correct.
             Otherwise, the password is incorrect.
          */
+    }
+
+
+    @PostMapping(path = "/login") //Map ONLY POST Requests
+    public @ResponseBody
+    String loginUser(@RequestParam String username
+            , @RequestParam String password) {
+        // @ResponseBody means the returned String is the response, not a view name
+        // @RequestParam means it is a parameter from the GET or POST request
+
+        User n = UserRepository.findByUsername(username);
+
+        String entered, hashEntered, correct;
+
+        correct = n.getHash();
+        entered = n.getSalt() + password;
+        hashEntered = DigestUtils.sha256Hex(entered);
+
+        System.out.println("salt: " + n.getSalt());
+
+        System.out.println("entered hash: " + hashEntered);
+        System.out.println("correct hash: " + correct);
+
+        if (correct.equals(hashEntered)) {
+            return n.getUserId().toString();
+        }
+
+
+        return "Incorrect Login";
+    }
+
+    @PostMapping(path= "/changepass") //Map ONLY POST Requests
+    public @ResponseBody String changePass (@RequestParam Long userId
+            , @RequestParam String password) {
+
+
+        User n = UserRepository.findOne(userId);
+        String salt_string = "????????";//RandomStringUtils.random(8); for some reason this isn't working --> stores salt as ????????
+        n.setSalt(salt_string);
+        String passSalt = salt_string + password;
+        String hashed = DigestUtils.sha256Hex(passSalt);
+        n.setHash(hashed);
+        UserRepository.save(n);
+
+        return "password changed!";
+
+
+
+
+
+
     }
 
 
