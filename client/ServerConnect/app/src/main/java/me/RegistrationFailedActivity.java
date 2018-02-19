6c80c245b17fbf3134832dea4a5b82da534c074e
asehnert <asehnert@iastate.edu>
2018-02-19 06:09:28 -0600

    Demo2 commits

---
+++ client/ServerConnect/app/src/main/java/me/RegistrationFailedActivity.java
@@ -0,0 +1,29 @@
+package com.example.austinsehnert.smartart;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class RegistrationFailedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_failed);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.failure_message);
+        textView.setText(message);
+    }
+
+    public void returnToUserRegistration(View view){
+        Intent backToUserRegistration = new Intent(this, MainActivity.class);
+        startActivity(backToUserRegistration);
+    }
+}
