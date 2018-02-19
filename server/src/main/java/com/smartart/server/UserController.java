ede2ce31fe25f958c1d5212dfa1fcd3e657563b2
rwendell <rwendell@iastate.edu>
2018-02-19 16:21:01 -0600

    response are now JSON and salting works

--- server/src/main/java/com/smartart/server/UserController.java
+++ server/src/main/java/com/smartart/server/UserController.java
@@ -3,6 +3,7 @@ package com.smartart.server;
 
 
 import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang.RandomStringUtils;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,12 +19,15 @@ public class UserController {
 
     private UserRepository UserRepository;
 
-    @RequestMapping(path = "/add", method = RequestMethod.POST, produces = "application/json") //Map ONLY POST Requests
+    @PostMapping(path = "/add",  produces = "application/json") //Map ONLY POST Requests
     public @ResponseBody
     String addNewUser(@RequestParam String username
             , @RequestParam String password) {
         // @ResponseBody means the returned String is the response, not a view name
         // @RequestParam means it is a parameter from the GET or POST request
+
+
+
 
 
         try {
@@ -33,14 +37,14 @@ public class UserController {
             User n = new User();
             n.setUsername(username);  //sets Username
             n.setAdmin(0);      //default to 0 -> false
-            String salt_string = "????????";//RandomStringUtils.random(8); for some reason this isn't working --> stores salt as ????????
+            String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~`!@#$%^&*()-_=+[{]}\\|;:\'\",<.>/?";
+            String salt_string = RandomStringUtils.random( 8, characters );
             n.setSalt(salt_string);
             String passSalt = salt_string + password;
             String hashed = DigestUtils.sha256Hex(passSalt);
             n.setHash(hashed);
             UserRepository.save(n);
-            //return "Saved New User";
-            //TODO: return JSON
+
 
             JSONObject userInfo = new JSONObject();
             userInfo.put("response", "user successfully added");
@@ -69,7 +73,7 @@ public class UserController {
     }
 
 
-    @RequestMapping(path = "/login", method = RequestMethod.POST, produces = "application/json") //Map ONLY POST Requests
+    @PostMapping(path = "/login", produces = "application/json") //Map ONLY POST Requests
     public @ResponseBody
     String loginUser(@RequestParam String username
             , @RequestParam String password) {
@@ -103,13 +107,15 @@ public class UserController {
         return fail.toString();
     }
 
-    @RequestMapping(path= "/changepass", method = RequestMethod.POST, produces = "application/json") //Map ONLY POST Requests
+    @PostMapping(path= "/changepass", produces = "application/json") //Map ONLY POST Requests
     public @ResponseBody String changePass (@RequestParam Long userId
             , @RequestParam String password) {
 
 
         User n = UserRepository.findOne(userId);
-        String salt_string = "????????";//RandomStringUtils.random(8); for some reason this isn't working --> stores salt as ????????
+
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~`!@#$%^&*()-_=+[{]}\\|;:\'\",<.>/?";
+        String salt_string = RandomStringUtils.random( 8, characters );
         n.setSalt(salt_string);
         String passSalt = salt_string + password;
         String hashed = DigestUtils.sha256Hex(passSalt);
