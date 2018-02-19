5c9e112dfae4f48d5346e3b2cb30edf613f21001
plicata <plicata@iastate.edu>
2018-02-18 20:21:55 -0600

    post request with  http://ip.jsontest.com

--- client/ServerConnect/app/src/main/java/me/paullicata/serverconnect/MainActivity.java
+++ client/ServerConnect/app/src/main/java/me/paullicata/serverconnect/MainActivity.java
@@ -3,23 +3,31 @@ package me.paullicata.serverconnect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     final String TAG = this.getClass().getSimpleName();
 
-    /*@Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String url = "http://proj-309-sb-2.cs.iastate.edu:80/select_data_sql.php";
+      /*  String url = "http://ip.jsontest.com";
+
+        JSONObject obj = new JSONObject();
 
         StringRequest request = new StringRequest(url, new Response.Listener<String>() {
             @Override
@@ -34,9 +42,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        MySingleton.getInstance(this).addToRequestQueue(request);
-
-    }*/
+        MySingleton.getInstance(this).addToRequestQueue(request);*/
 
 
+
+    final TextView mTxtDisplay;
+    ImageView mImageView;
+    mTxtDisplay = (TextView) findViewById(R.id.txtDisplay);
+    String url = "http://ip.jsontest.com";
+
+    JsonObjectRequest jsObjRequest = new JsonObjectRequest
+            (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                @Override
+                public void onResponse(JSONObject response) {
+                    mTxtDisplay.setText("Response: " + response.toString());
+                }
+            }, new Response.ErrorListener() {
+
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    // TODO Auto-generated method stub
+
+                }
+            });
+
+// Access the RequestQueue through your singleton class.
+    MySingleton.getInstance(this).addToRequestQueue(jsObjRequest);
+
+    }
 }
