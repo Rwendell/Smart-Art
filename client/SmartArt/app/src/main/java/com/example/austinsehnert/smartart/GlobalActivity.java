4124769931c7b877b51550213a5289acf6087ef2
asehnert <asehnert@iastate.edu>
2018-04-09 11:03:33 -0500

    demo 3

--- client/SmartArt/app/src/main/java/com/example/austinsehnert/smartart/GlobalActivity.java
+++ client/SmartArt/app/src/main/java/com/example/austinsehnert/smartart/GlobalActivity.java
@@ -1,5 +1,6 @@
 package com.example.austinsehnert.smartart;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,6 +8,8 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.UUID;
 import android.provider.MediaStore;
 import android.app.AlertDialog;
@@ -15,8 +18,11 @@ import android.content.DialogInterface;
 import android.view.View.OnClickListener;
 import android.widget.Toast;
 
+import com.example.austinsehnert.smartart.utils.ArrayCopy;
+import com.example.austinsehnert.smartart.utils.ImgUtils;
 
-public class GlobalActivity extends AppCompatActivity implements OnClickListener{
+
+public class GlobalActivity extends AppCompatActivity implements OnClickListener {
 
     private Draw canvas;
 
@@ -32,9 +38,14 @@ public class GlobalActivity extends AppCompatActivity implements OnClickListener
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_global);
 
+
+        //String fileLoc = loadArtboard();
+
+
         canvas = findViewById(R.id.drawing);
+
         LinearLayout paintLayout = findViewById(R.id.paint_colors);
-        currentColor = (ImageButton)paintLayout.getChildAt(0);
+        currentColor = (ImageButton) paintLayout.getChildAt(0);
         currentColor.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
 
         smallBrush = getResources().getInteger(R.integer.small_size);
@@ -52,6 +63,8 @@ public class GlobalActivity extends AppCompatActivity implements OnClickListener
         newBtn = findViewById(R.id.new_btn);
         newBtn.setOnClickListener(this);
 
+
+        /*
         AlertDialog.Builder newDialog = new AlertDialog.Builder(this);
         newDialog.setTitle("New drawing");
         newDialog.setMessage("Start new drawing (you will lose the current drawing)?");
@@ -68,28 +81,30 @@ public class GlobalActivity extends AppCompatActivity implements OnClickListener
         });
         newDialog.show();
 
+        */
+
         saveBtn = findViewById(R.id.save_btn);
         saveBtn.setOnClickListener(this);
 
     }
 
-    public void paintClicked(View view){
-        if(view != currentColor){
-            ImageButton imgView = (ImageButton)view;
+    public void paintClicked(View view) {
+        if (view != currentColor) {
+            ImageButton imgView = (ImageButton) view;
             String color = view.getTag().toString();
 
             canvas.setColor(color);
             imgView.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
             currentColor.setImageDrawable(getResources().getDrawable(R.drawable.paint));
-            currentColor=(ImageButton)view;
+            currentColor = (ImageButton) view;
 
             canvas.setBrushSize(canvas.getLastBrushSize());
         }
     }
 
     @Override
-    public void onClick(View view){
-        if(view.getId() == R.id.draw_btn) {
+    public void onClick(View view) {
+        if (view.getId() == R.id.draw_btn) {
             final Dialog brushDialog = new Dialog(this);
             brushDialog.setTitle("Brush size:");
 
@@ -130,15 +145,13 @@ public class GlobalActivity extends AppCompatActivity implements OnClickListener
 
 
             brushDialog.show();
-        }
-
-        else if(view.getId()==R.id.erase_btn){
+        } else if (view.getId() == R.id.erase_btn) {
             final Dialog brushDialog = new Dialog(this);
             brushDialog.setTitle("Eraser size:");
             brushDialog.setContentView(R.layout.brush_chooser);
 
             ImageButton smallBtn = brushDialog.findViewById(R.id.small_brush);
-            smallBtn.setOnClickListener(new OnClickListener(){
+            smallBtn.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     canvas.setErase(true);
@@ -147,7 +160,7 @@ public class GlobalActivity extends AppCompatActivity implements OnClickListener
                 }
             });
             ImageButton mediumBtn = brushDialog.findViewById(R.id.medium_brush);
-            mediumBtn.setOnClickListener(new OnClickListener(){
+            mediumBtn.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     canvas.setErase(true);
@@ -156,7 +169,7 @@ public class GlobalActivity extends AppCompatActivity implements OnClickListener
                 }
             });
             ImageButton largeBtn = brushDialog.findViewById(R.id.large_brush);
-            largeBtn.setOnClickListener(new OnClickListener(){
+            largeBtn.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     canvas.setErase(true);
@@ -166,28 +179,26 @@ public class GlobalActivity extends AppCompatActivity implements OnClickListener
             });
 
             brushDialog.show();
-        }
-
-        else if(view.getId()==R.id.new_btn){
+        } else if (view.getId() == R.id.new_btn) {
             AlertDialog.Builder newDialog = new AlertDialog.Builder(this);
             newDialog.setTitle("New drawing");
             newDialog.setMessage("Start new drawing (you will lose the current drawing)?");
-            newDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener(){
-                public void onClick(DialogInterface dialog, int which){
+            newDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
                     canvas.startNew();
                     dialog.dismiss();
                 }
             });
-            newDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
-                public void onClick(DialogInterface dialog, int which){
+            newDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
                     dialog.cancel();
                 }
             });
             newDialog.show();
-        }
+        } else if (view.getId() == R.id.save_btn) {
 
-        else if(view.getId()==R.id.save_btn) {
 
+            /*
             AlertDialog.Builder saveDialog = new AlertDialog.Builder(this);
             saveDialog.setTitle("Save drawing");
             saveDialog.setMessage("Save drawing to device Gallery?");
@@ -219,9 +230,16 @@ public class GlobalActivity extends AppCompatActivity implements OnClickListener
 
             canvas.destroyDrawingCache();
         }
+
+        */
+
+
+        }
+
+
+
+
     }
-
-
 
 
 }
