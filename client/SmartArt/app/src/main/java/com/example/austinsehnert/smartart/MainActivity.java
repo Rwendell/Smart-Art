4020429cd944ab5821b8046ebbbd92abb278a592
plicata <plicata@iastate.edu>
2018-04-10 13:26:38 -0500

    websockets send/receive images

--- client/SmartArt/app/src/main/java/com/example/austinsehnert/smartart/MainActivity.java
+++ client/SmartArt/app/src/main/java/com/example/austinsehnert/smartart/MainActivity.java
@@ -4,44 +4,9 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.Volley;
-import com.example.austinsehnert.smartart.app.AppController;
-
-import org.json.JSONArray;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = MainActivity.class.getSimpleName();
 
 
     @Override
@@ -49,11 +14,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent signIn = new Intent(this, SignInActivity.class);
-        startActivity(signIn);
+        /*Intent signIn = new Intent(this, SignInActivity.class);
+        startActivity(signIn);*/
 
-        Intent socketTest = new Intent(this, WebSocket.class);
-        startActivity(socketTest);
+        Intent socket = new Intent(this, WebSocket.class);
+        startActivity(socket);
+
+        //WebSocket w = new WebSocket();
+       // w.connectWebSocket();
+
+        //w.sendMessage("{\"firstName\": \"John\"}");
 
     }
 }
