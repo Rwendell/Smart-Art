3286e0071dc9760656846eed6e31a9f8cf38657a
asehnert <asehnert@iastate.edu>
2018-04-20 04:57:30 -0500

    small changes to sign in and global

--- client/SmartArt/app/src/main/java/com/example/austinsehnert/smartart/SignInActivity.java
+++ client/SmartArt/app/src/main/java/com/example/austinsehnert/smartart/SignInActivity.java
@@ -46,11 +46,11 @@ public class SignInActivity extends AppCompatActivity {
         Intent registrationSuccessful = new Intent(this, DisplayMessageActivity.class);
         Intent registrationFailed = new Intent(this, RegistrationFailedActivity.class);
 
-        String url1 = "http://proj-309-sb-2.cs.iastate.edu:8080/user/login?username=meme&password=lol";
-
-        String url = "http://ip.jsontest.com";
-
-        String tag_json_obj = "json_obj_req";
+//        String url1 = "http://proj-309-sb-2.cs.iastate.edu:8080/user/login?username=meme&password=lol";
+//
+//        String url = "http://ip.jsontest.com";
+//
+//        String tag_json_obj = "json_obj_req";
 
         final TextView mTxtDisplay;
         mTxtDisplay = findViewById(R.id.username);
@@ -72,9 +72,6 @@ public class SignInActivity extends AppCompatActivity {
 
 
 
-
-
-
         JsonObjectRequest jsObjRequest = new JsonObjectRequest
                 (Request.Method.POST, all, null, new Response.Listener<JSONObject>() {
 
@@ -84,7 +81,7 @@ public class SignInActivity extends AppCompatActivity {
 
                         Log.d("RESPONSE", "Response: " + response.toString());
                         rsp = response;
-                        System.out.println(rsp.toString());
+                        System.out.println("RSP: " + rsp.toString());
                         response_string = rsp.toString();
                         //System.out.println(response.toString());
 
@@ -102,14 +99,14 @@ public class SignInActivity extends AppCompatActivity {
         // Access the RequestQueue through your singleton class.
         //MySingleton.getInstance(this).addToRequestQueue(jsObjRequest);
 
-       // AppController.getInstance().addToRequestQueue(jsObjRequest, tag_json_obj);
+        // AppController.getInstance().addToRequestQueue(jsObjRequest, tag_json_obj);
 
         //startActivity(registrationSuccessful);
 
 
         String main_response = response_string;
         System.out.println(response_string + "TEST");
-        //System.out.println(main_response);
+        System.out.println("MAIN RESPONSE: " + main_response);
         //System.out.println(u_response.get("response"));
 
         if (main_response.contains("Successful")) {
@@ -119,6 +116,8 @@ public class SignInActivity extends AppCompatActivity {
         } else if (main_response.contains("Incorrect Login")) {
             startActivity(registrationFailed);
         }
+
+        startActivity(registrationSuccessful);
 
     }
 
